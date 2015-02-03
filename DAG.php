d0f424f0c6d822d05c093e648267b0ffd59285d1
yyinsomnia <yyinsomnia@gmail.com>
2015-02-03 19:53:48 +0800

    more

--- DAG.php
+++ DAG.php
@@ -26,6 +26,14 @@ function topological_sort($G)
 	return $res;
 }
 
+function relax($u, $v, &$shortest, &$pred)
+{
+	if ($shortest[$u] + weight($u, $v) < $shortest[$v]) {
+		$shortest[$v] = $shortest[$u] + weight($u, $v);
+		$pred[$v] = $u;
+	}
+}
+
 $adjacency_list = [1 => [3], 2 => [4], 3 => [4, 5], 4 => [6], 5 => [6], 6 => [7, 11], 7 => [8], 8 => [13], 9 => [10], 10 => [11], 11 => [12], 12 => [13], 13 => [14], 14 => []];
 
 

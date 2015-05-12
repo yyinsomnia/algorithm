653b5f8e46351723c0b7bf4dccbe607dbdba8785
yyinsomnia <yyinsomnia@gmail.com>
2015-05-12 11:52:20 +0800

    insertion sort

---
+++ sort_insertion_01.c
@@ -0,0 +1,11 @@
+void InsertionSort(Element A[], int N)
+{
+	int j, P;
+	for (P = 1; P < N; P++)
+	{
+		Tmp = A[P];
+		for (j = P; j > 0 && A[j - 1] > Tmp; j--)
+			A[j] = A[j - 1];
+		A[j] = Tmp;
+	}
+}

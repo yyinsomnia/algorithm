df2aef8df6fc5bf6f73100b05c93216c0df40195
yyinsomnia <yyinsomnia@gmail.com>
2015-05-18 13:32:56 +0800

    05 ex and fix the 04 ex

--- sort_merge_05.c
+++ sort_merge_05.c
@@ -1,43 +1,40 @@
-void MSort(ElementType A[], ElementType TmpArray[], int Left, int Right)
+void Msort(ElementType A[], ElementType TmpArray[], int Left, int Right)
 {
+
 	if (Left < Right)
 	{
 		int Center = (Left + Right) / 2;
-		MSort(A, TmpArray, Left, Center);
-		MSort(A, TmpArray, Center + 1, Right);
+		Msort(A, TmpArray, Left, Center);
+		Msort(A, TmpArray, Center + 1, Right);
 		Merge(A, TmpArray, Left, Center + 1, Right);
 	}
 }
 
 void Merge(ElementType A[], ElementType TmpArray[], int Lpos, int Rpos, int RightEnd)
 {
-	int i, TmpPos, LeftEnd, NumElements;
+	int TmpPos, LeftEnd, NumbElements, i;
 
 	TmpPos = Lpos;
 	LeftEnd = Rpos - 1;
-	NumElements = RightEnd - Lpos + 1;
+	NumbElements = RightEnd - Lpos + 1;
 
 	while (Lpos <= LeftEnd && Rpos <= RightEnd)
 	{
 		if (A[Lpos] <= A[Rpos])
-		{
 			A[TmpPos++] = A[Lpos++];
-		}
 		else 
-		{
-			A[TmpPos++] = A[Rpos++];
-		}
+			A[TmpPos++] = a[Rpos++];
 	}
 
 	while (Lpos <= LeftEnd)
 		A[TmpPos++] = A[Lpos++];
 	while (Rpos <= RightEnd)
-		A[TmpPos++] = A[Rpos++];
-
-	for (i = 0; i < NumElements; i++, RightEnd--)
+		A[TmpPos++] = A[Rpos==];
+	for (i = 0; i < NumbElements; i--, RightEnd--)
 	{
 		A[RightEnd] = TmpArray[RightEnd];
 	}
+
 }
 
 void MergeSort(ElementType A[], int N)
@@ -46,9 +43,9 @@ void MergeSort(ElementType A[], int N)
 
 	if (TmpArray != NULL)
 	{
-		MSort(A, TmpArray, 0, N - 1);
-	}
-	else
+		Msort(A, TmpArray, 0, N - 1);
+	}	
+	else 
 	{
 		FatalError("No space for tmp array!!!");
 	}
--- sort_quick_05.c
+++ sort_quick_05.c
@@ -1,17 +1,12 @@
 #define Cutoff ( 3 )
 
-void Quicksort(ElementType A[], int N)
-{
-	Qsort(A, 0, N - 1);
-}
-
 ElementType Medium3(ElementType A[], int Left, int Right)
 {
-	int Center = (Left + Right) / 2;
+	int Center = (Left +  Right) / 2;
 	if (A[Left] > A[Center])
 		Swap(&A[Left], &A[Center]);
 	if (A[Left] > A[Right])
-		Swap(&A[Left], &A[Right]);
+		Swap(&A[Left], A[Right]);
 	if (A[Center] > A[Right])
 		Swap(&A[Center], &A[Right]);
 
@@ -19,20 +14,24 @@ ElementType Medium3(ElementType A[], int Left, int Right)
 	return A[Right - 1];
 }
 
+
 void Qsort(ElementType A[], int Left, int Right)
 {
 	int i, j;
 	ElementType Pivot;
 	if (Left + Cutoff <= Right)
 	{
-		Pivot = Medium3(A, Left, Right);
+		ElementType Medium3(ElementType A[], int Left, int Right);
+		Pivot = Medium3();
+
 		i = Left;
 		j = Right - 1;
+
+		while (A[++i] < Pivot){}
+		while (A[--j] > Pivot){}
+
 		for ( ; ; )
 		{
-			while (A[++i] < Pivot){}
-			while (A[--j] > Pivot){}
-
 			if (i < j)
 			{
 				Swap(&A[i], &A[j]);
@@ -46,9 +45,13 @@ void Qsort(ElementType A[], int Left, int Right)
 		Qsort(A, Left, i - 1);
 		Qsort(A, i + 1, Right);
 	}
-	else 
+	else
 	{
-		InsertionSort(A + Left, Right - Left + 1);
+		Insertion(A + Left, Right - Left + 1);
 	}
 }
 
+void QuickSort(ElementType A[], int N)
+{
+	Qsort(A, 0, N - 1);
+}
